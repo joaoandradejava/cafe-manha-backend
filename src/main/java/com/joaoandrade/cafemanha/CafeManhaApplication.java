36c2ef1112020155249e0966778572664d39d7fb
joaoandradejava <joaoandrade@cliniconect.com.br>
2021-10-11 18:51:14 -0300

    Primeiro projeto

---
+++ src/main/java/com/joaoandrade/cafemanha/CafeManhaApplication.java
@@ -0,0 +1,13 @@
+package com.joaoandrade.cafemanha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeManhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeManhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaoandrade/cafemanha/CafeManhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaoandrade.cafemanha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeManhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
